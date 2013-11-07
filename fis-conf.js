a412a2a6c35e2b51b8609097e6372122c92a0b92
张云龙 <zhangyunlong@baidu.com>
2013-11-07 09:48:17 +0800

    Update fis-conf.js

--- fis-conf.js
+++ fis-conf.js
@@ -78,8 +78,8 @@ fis.config.merge({
             {
                 //前端模板
                 reg : '**.tmpl',
-                //当做类html文件处理，可以识别<img src="xxx"/>等资源定位标识
-                isHtmlLike : true,
+                //当做类js文件处理，可以识别__inline, __uri等资源定位标识
+                isJsLike : true,
                 //只是内嵌，不用发布
                 release : false
             },
